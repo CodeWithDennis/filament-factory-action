b4ff701f119e5f69257ad495f68c3166d081e473
CodeWithDennis <CodeWithDennis@users.noreply.github.com>
2023-10-10 17:32:54 +0000

    Fix styling

--- src/FactoryActionServiceProvider.php
+++ src/FactoryActionServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace CodeWithDennis\FactoryAction;
 
+use CodeWithDennis\FactoryAction\Commands\FactoryActionCommand;
+use CodeWithDennis\FactoryAction\Testing\TestsFactoryAction;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use CodeWithDennis\FactoryAction\Commands\FactoryActionCommand;
-use CodeWithDennis\FactoryAction\Testing\TestsFactoryAction;
 
 class FactoryActionServiceProvider extends PackageServiceProvider
 {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace CodeWithDennis\FactoryAction\Tests;
 
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
+use CodeWithDennis\FactoryAction\FactoryActionServiceProvider;
 use Filament\Actions\ActionsServiceProvider;
 use Filament\FilamentServiceProvider;
 use Filament\Forms\FormsServiceProvider;
@@ -18,7 +19,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use CodeWithDennis\FactoryAction\FactoryActionServiceProvider;
 
 class TestCase extends Orchestra
 {
