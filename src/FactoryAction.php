d6981de7965a294b5061cfd406276ebbe8e930a7
Dennis Elsinga <personal.elsinga@gmail.com>
2023-10-23 19:41:30 +0200

    Resolve conflicts

--- src/FactoryAction.php
+++ src/FactoryAction.php
@@ -2,23 +2,27 @@
 
 namespace CodeWithDennis\FactoryAction;
 
+use App\Models\Profile;
 use Closure;
 use Filament\Actions\Action;
-use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use ReflectionClass;
+use ReflectionMethod;
 
 class FactoryAction extends Action
 {
-    protected array $hasManyRelations = [];
-
-    protected array $belongsToManyRelations = [];
+    protected array $relationships = [];
 
     public static function getDefaultName(): ?string
     {
         return 'generate';
     }
 
-    public function action(Closure | string | null $action): static
+    public function action(Closure|string|null $action): static
     {
         if ($action !== 'createFactory') {
             throw new \Exception('You\'re unable to override the action for this plugin');
@@ -29,7 +33,7 @@ class FactoryAction extends Action
         return $this;
     }
 
-    public function form(array | Closure | null $form): static
+    public function form(array|Closure|null $form): static
     {
         $this->form = $this->getDefaultForm();
 
@@ -38,67 +42,73 @@ class FactoryAction extends Action
 
     protected function getDefaultForm(): array
     {
+        // TODO: Allow to disable relationships
+        $fields = [];
+
+        foreach ($this->relationships as $relationship) {
+            $relationshipName = (new ReflectionClass($relationship['name']))->getShortName();
+
+            $fields[] = TextInput::make($relationship['name'])
+                ->default(0)
+                ->numeric()
+                ->label(__($relationshipName))
+                ->required();
+        }
+
         return [
             TextInput::make('quantity')
-                ->label(fn ($livewire) => __('Amount of') . ' ' . $livewire->getTable()->getPluralModelLabel())
+                ->label(fn($livewire) => (new ReflectionClass($livewire->getModel()))->getShortName())
                 ->numeric()
                 ->rules('numeric|min:1')
                 ->default(1)
                 ->columns()
                 ->required(),
 
-            TextInput::make('relational_quantity')
-                ->label(fn ($livewire) => __('Amount of relational models'))
-                ->numeric()
-                ->visible(fn () => count($this->belongsToManyRelations) || count($this->hasManyRelations))
-                ->rules('numeric|min:1')
-                ->default(1)
-                ->columns()
-                ->required(),
-
-            Select::make('attach')
-                ->options(fn () => collect($this->belongsToManyRelations)
-                    ->mapWithKeys(fn ($value) => [$value => __(class_basename($value))]))
-                ->visible(fn () => count($this->belongsToManyRelations))
-                ->native(false)
-                ->multiple(),
-
-            Select::make('create')
-                ->options(fn () => collect($this->hasManyRelations)
-                    ->mapWithKeys(fn ($value) => [$value => __(class_basename($value))]))
-                ->visible(fn () => count($this->hasManyRelations))
-                ->native(false)
-                ->multiple(),
+            Fieldset::make(__('Relationships'))
+                ->schema($fields)
         ];
-    }
-
-    public function hasMany(array $relations): static
-    {
-        $this->hasManyRelations = $relations;
-
-        return $this;
-    }
-
-    public function belongsToMany(array $relations): static
-    {
-        $this->belongsToManyRelations = $relations;
-
-        return $this;
     }
 
     protected function setUp(): void
     {
         parent::setUp();
 
+        $model = new Profile(); // TODO: Replace with actual model of the ResourcePage
+
+        foreach ((new ReflectionClass($model))->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
+            if (
+                $method->class !== get_class($model) ||
+                !empty($method->getParameters()) ||
+                $method->getName() === __FUNCTION__
+            ) {
+                continue;
+            }
+
+            $return = $method->invoke($model);
+
+            if ($return instanceof Relation) {
+                $relation = new ReflectionClass($return);
+
+                if (in_array($relation->name, [HasMany::class, BelongsToMany::class])) {
+                    $relatedModelName = (new ReflectionClass($return->getRelated()))->getName();
+
+                    $this->relationships[$method->getName()] = [
+                        'name' => $relatedModelName,
+                        'relation' => $relation,
+                    ];
+                }
+            }
+        }
+
         $this->icon('heroicon-o-cog-8-tooth')
             ->color('warning')
-            ->hidden(fn () => app()->isProduction())
+            ->hidden(fn() => app()->isProduction())
             ->form($this->getDefaultForm())
             ->modalIcon('heroicon-o-cog-8-tooth')
             ->color('success')
-            ->modalWidth('md')
+            ->modalWidth('2xl')
             ->modalAlignment('center')
-            ->modalHeading(fn ($livewire) => __('Generate'))
+            ->modalHeading(fn($livewire) => __('Generate'))
             ->modalDescription(__('This action will create new records in the database. Are you sure you would like to proceed?'))
             ->modalFooterActionsAlignment('right')
             ->closeModalByClickingAway(false)
@@ -109,17 +119,12 @@ class FactoryAction extends Action
     {
         return function (array $data, $livewire) {
             $factory = $livewire->getModel()::factory($data['quantity']);
+            unset($data['quantity']);
 
-            foreach ($this->hasManyRelations as $hasManyRelation) {
-                if (isset($data['create']) && in_array($hasManyRelation, $data['create'])) {
-                    $factory = $factory->has($hasManyRelation::factory()->count($data['relational_quantity']));
-                }
-            }
-
-            foreach ($this->belongsToManyRelations as $belongsToManyRelation) {
-                if (isset($data['attach']) && in_array($belongsToManyRelation, $data['attach'])) {
-                    $models = $belongsToManyRelation::inRandomOrder()->limit($data['relational_quantity'])->get();
-                    $factory = $factory->hasAttached($models);
+            foreach ($data as $key => $value) {
+                $type = collect($this->relationships)->firstWhere('name', $key);
+                if ($type && in_array($type['relation']->name, [HasMany::class, BelongsToMany::class])) {
+                    $factory = $factory->has($key::factory()->count($value));
                 }
             }
 
